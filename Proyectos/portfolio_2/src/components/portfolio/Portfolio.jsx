f736280120b58ca513393e39c0226adb3cbdc9b8
vdanielg <adgv.99@hotmail.com>
2022-08-01 19:44:30 -0500

    Portfolio añadido

--- Proyectos/portfolio_2/src/components/portfolio/Portfolio.jsx
+++ Proyectos/portfolio_2/src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,91 @@
-import React from "react"
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
+      <div className="titulo">
+        <h3>Recent projects</h3>
+        <h2>My portfolio</h2>
+      </div>
       <div className="container portfolio_container">
-        <div className="titulo">
-          <h3>Recent projects</h3>
-          <h2>My portfolio</h2>
-        </div>
+        <article className="project">
+          <div className="img-project">
+            <img src="" alt="image" />
+          </div>
+          <h2>Title Project</h2>
+          <div className="btn-project">
+            <a href="" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="project">
+          <div className="img-project">
+            <img src="" alt="image" />
+          </div>
+          <h2>Title Project</h2>
+          <div className="btn-project">
+            <a href="" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="project">
+          <div className="img-project">
+            <img src="" alt="image" />
+          </div>
+          <h2>Title Project</h2>
+          <div className="btn-project">
+            <a href="" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="project">
+          <div className="img-project">
+            <img src="" alt="image" />
+          </div>
+          <h2>Title Project</h2>
+          <div className="btn-project">
+            <a href="" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="project">
+          <div className="img-project">
+            <img src="" alt="image" />
+          </div>
+          <h2>Title Project</h2>
+          <div className="btn-project">
+            <a href="" className="btn" target="_blank">
+              GitHub
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Portfolio;
